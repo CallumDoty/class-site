43c586f6560b665d9d0feb65c14bac47cf215a30
CallumDoty <dotycf@vcu.edu>
2022-10-31 17:40:32 -0400

    Contro Chaos

--- control/scripts.js
+++ control/scripts.js
@@ -1,34 +1,163 @@
 
 
-$(document).ready(function(){
-  $("button").click(function(){
-    $("div").animate({
-      left: '250px',
-      opacity: '0.5',
-      height: '150px',
-      width: '150px'
-    });
-  });
+
+// a variable to store the number of times the button has been clicked
+var clicks = 0;
+
+$(".button").on("click",function() {
+  $("button").clone().appendTo("body");
+  $("body").clone().appendTo("button");
+  $("button").clone().appendTo("body");
+  $("button").clone().appendTo("button");
+    if(clicks === 0){
+        animation1();
+    } 
+    
+    if(clicks === 1){
+        animation2();
+    }
+     
+    if(clicks === 2){
+        animation3();
+    }
+
+    if(clicks === 3){
+      animation4();
+    } 
+  
+    if(clicks === 4){
+      animation5();
+    }
+   
+    if(clicks === 5){
+      animation6();
+    }
+
+    if(clicks === 6){
+      animation1();
+    } 
+  
+   if(clicks === 7){
+      animation2();
+    }
+   
+    if(clicks === 8){
+      animation3();
+    }
+
+    if(clicks === 9){
+    animation4();
+    } 
+
+    if(clicks === 10){
+    animation5();
+    }
+ 
+    if(clicks === 11){
+    animation6();
+    }
+
+
+    
+    // ...
+    
+    // this increments our clicks variable by 1 
+    // each time the button is clicked
+    clicks++;
 });
 
-$(document).ready(function(){
-  $("button").click(function(){
-    $("div").animate({
-      right: '250px',
-      opacity: '100',
-      height: '1500px',
-      width: '1500px'
-    });
+function animation1() {
+  $("div").animate({
+    
+    opacity: '100',
+    height: '850px',
+    width: '850px',
+    
   });
-});
+}
 
-$(document).ready(function(){
-  $("button").click(function(){
-    $("div").animate({
-      left: '400px',
-      opacity: '100',
-      height: '100px',
-      width: '100px'
-    });
+function animation2() {
+  $("div").animate({
+    
+    opacity: '100',
+    height: '500px',
+    width: '500px'
   });
-});
+}
+
+function animation3() {
+  $("div").animate({
+    
+    opacity: '100',
+    height: '100px',
+    width: '100px'
+  });
+}
+
+function animation4() {
+  $("div").animate({
+  
+    opacity: '100',
+    height: '100px',
+    width: '100px'
+  });
+}
+
+function animation5() {
+  $("div").animate({
+   
+    opacity: '100',
+    height: '1900px',
+    width: '1900px'
+  });
+}
+function animation6() {
+  $("div").animate({
+   
+    opacity: '100',
+    height: '1300px',
+    width: '1300px'
+  });
+}
+
+function animation7() {
+  $("div").animate({
+    
+    opacity: '100',
+    height: '10px',
+    width: '10px'
+  });
+}
+function animation8() {
+  $("div").animate({
+   
+    opacity: '100',
+    height: '1070px',
+    width: '1070px'
+  });
+}
+
+function animation9() {
+  $("div").animate({
+  
+    opacity: '100',
+    height: '100px',
+    width: '100px'
+  });
+}
+function animation10() {
+  $("div").animate({
+  
+    opacity: '100',
+    height: '1000px',
+    width: '1000px'
+  });
+}
+
+function animation11() {
+  $("div").animate({
+    opacity: '100',
+    height: '1100px',
+    width: '1100px'
+  });
+}
